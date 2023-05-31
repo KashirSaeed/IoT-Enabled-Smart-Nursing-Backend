e8758ff2554234574fe985ac0b0fff83ac51dd48
AbdulAhad696 <abdulahad_212@outlook.com>
2023-05-31 14:35:44 +0500

    Update wsgi.py

--- smartNursingBackend/wsgi.py
+++ smartNursingBackend/wsgi.py
@@ -26,7 +26,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartNursingBackend.settings')
 
 app = get_wsgi_application()
 # mqInstance = HiveMqtt()
-logger.initLogger()
+# logger.initLogger()
 initPusher()
 
 
