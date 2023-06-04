3705ff40e98b8251604d833454d94524d64a1cb3
KashirSaeed <92178175+KashirSaeed@users.noreply.github.com>
2023-06-04 15:16:55 +0500

    Done with threading
    Done with threading

--- API_Handler/views.py
+++ API_Handler/views.py
@@ -4,27 +4,13 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 from django.http import JsonResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from rest_framework.views import APIView
 import influxdb_client
-from influxdb_client.client.write_api import SYNCHRONOUS
-# Create your views here.
 from django.http import HttpResponse
 import datetime
-# import the logging library
 import logging
-
-from django.http import HttpResponse
-from influxdb_client import InfluxDBClient, Point
-from influxdb_client.client.write_api import SYNCHRONOUS
-from django.http import JsonResponse
-import json
-
-# import datetime
-# import the logging library
-import logging
-# Get an instance of a logger
+# ------------Get an instance of a logger------------
 logger = logging.getLogger(__name__)
-# creating .log file per day
+# -----------creating .log file per day--------
 from logging.handlers import TimedRotatingFileHandler
 logname = "logs/logsContainer.log"
 handler = TimedRotatingFileHandler(logname, when="midnight", backupCount=30)
@@ -32,11 +18,13 @@ handler.suffix = "%Y%m%d"
 logger.addHandler(handler)
 
 # ---------Credentials of influxdb-------------
-bucket = "Users"
+databucket = "Users"
+# databucket = "Object Detection"
 org = "1936be69c64da4d7"
 token = "R4yVXBDI84LlpaZijvjNMrhl-8m-67S_gUNhON9CXISLLSEwKP4Oaeykw8UaF-wq5rQs4_kismihsVNBCC3vVQ=="
 url = "https://us-east-1-1.aws.cloud2.influxdata.com"
 measurement = "User"
+client = InfluxDBClient(url=url, token=token, org=org)
 
 # --------------Posting the data in influxdb-----------
 @csrf_exempt
@@ -58,7 +46,7 @@ def postUserData(request):
     client = InfluxDBClient(url=url, token=token, org=org)
     # ----------Query for checking whether user with same email exist--------
     query_api = client.query_api()
-    query = f'from(bucket:"{bucket}")|> range(start: -1y)|> filter(fn:(r) => r._measurement == "{measurement}" and r.email == "{myEmail}" )'
+    query = f'from(bucket:"{databucket}")|> range(start: -1y)|> filter(fn:(r) => r._measurement == "{measurement}" and r.email == "{myEmail}" )'
     # ----------append data in array-----------
     result = query_api.query(org=org, query=query)
     results = []
@@ -69,11 +57,11 @@ def postUserData(request):
     if(len(results) == 0 ):
         # ----------code for posting data----------
         # Define the data to be written
-        data = Point(measurement).field("isAuthenticatedByGoogle", isAuthenticatedByGoogle).field("username", myUsername).tag("email",myEmail).field("password", myPassword).time(datetime.datetime.utcnow().isoformat() + 'Z')
+        data = Point(measurement).tag("isAuthenticatedByGoogle", isAuthenticatedByGoogle).field("username", myUsername).tag("email",myEmail).tag("password", myPassword).time(datetime.datetime.utcnow().isoformat() + 'Z')
         # Create a write API instance
         write_api = client.write_api(write_options=SYNCHRONOUS)
         # Write the data to the bucket
-        write_api.write(bucket=bucket, record=data)
+        write_api.write(bucket=databucket, record=data)
         # Close the write API
         write_api.close()
         print("--------------user registered successfully------------")
@@ -105,17 +93,20 @@ def getSpecificUser(request, *args, **kwargs):
     if(isAuthenticatedByGoogle == "false"):
         # -----------query for reading data--------
         query_api = client.query_api()    
-        query = f'from(bucket:"{bucket}") \
+        query = f'from(bucket:"{databucket}") \
         |> range(start: -1y) \
-        |> filter(fn:(r) => r._measurement == "{measurement}" and r.email == "{myEmail}"   and r._field == "password"  and r._value == "{myPassword}" )'
+        |> filter(fn:(r) => r._measurement == "{measurement}" and r.email == "{myEmail}"   and r.password == "{myPassword}"  and r.isAuthenticatedByGoogle == "{isAuthenticatedByGoogle}"  )' 
+  
 
     elif(isAuthenticatedByGoogle == "true"):
         # -----------query for reading data--------
         query_api = client.query_api()    
-        query = f'from(bucket:"{bucket}") \
+        query = f'from(bucket:"{databucket}") \
         |> range(start: -1y) \
-        |> filter(fn:(r) => r._measurement == "{measurement}" and r.email == "{myEmail}" )'
+        |> filter(fn:(r) => r._measurement == "{measurement}" and r.email == "{myEmail}" and r.isAuthenticatedByGoogle == "{isAuthenticatedByGoogle}"  )'
+        
 
+        
     result = query_api.query(org=org, query=query)
     results = []
     for table in result:
@@ -128,35 +119,6 @@ def getSpecificUser(request, *args, **kwargs):
     else:
         return JsonResponse({"response":"true"},safe=False)
 
-
-# @csrf_exempt
-# def addingUsertype(request , *args, **kwargs)  :
-#     print("I am in--------")
-#     usertype = kwargs['usertype']
-    
-#     client = InfluxDBClient(url="https://us-east-1-1.aws.cloud2.influxdata.com", token="R4yVXBDI84LlpaZijvjNMrhl-8m-67S_gUNhON9CXISLLSEwKP4Oaeykw8UaF-wq5rQs4_kismihsVNBCC3vVQ==", org="1936be69c64da4d7")
-
-#     # Write API instance
-#     write_api = client.write_api(write_options=SYNCHRONOUS)
-
-#     # Prepare the data update
-#     measurement = 'User'
-#     # tags = {'email': 'umairahmedpaki7@gmail.com' }
-#     # fields = {'usertype': usertype}
-#     # timestamp = '2023-05-28T09:49:20.980Z'
-
-#     # Construct the data point with the new field
-#     point  =Point(measurement).tag("email", 'umairahmedpaki7@gmail.com').field('usertype', usertype).time('2023-05-28T10:19:49.889Z')
-
-#     # Write the data point to InfluxDB
-#     write_api.write(bucket='Users', record=point)
-
-#     # Close the InfluxDB client
-#     client.close()
-   
-#     return JsonResponse({},safe=False)
-# example/views.py
-# from datetime import datetime
 
 def index(request):
     logger.warning('Homepage was accessed at '+str(datetime.now())+' hours!')
@@ -171,17 +133,10 @@ def index(request):
     '''
     return HttpResponse(html)
 
-# Declaring an object with parameters initialised
-url = "https://us-east-1-1.aws.cloud2.influxdata.com"
-org = "1936be69c64da4d7"
-token = "R4yVXBDI84LlpaZijvjNMrhl-8m-67S_gUNhON9CXISLLSEwKP4Oaeykw8UaF-wq5rQs4_kismihsVNBCC3vVQ=="
-databucket = "Object Detection"
-client = InfluxDBClient(url=url, token=token, org=org)
+
 
 def count_influx(request):
-
     query = f'from(bucket:"{databucket}")|> range(start: -30d)|> filter(fn: (r) => r._measurement == "objectDetection")|> count()'
-
     result = client.query_api().query(query)
     try:
         response = result[0].records[0].values['_value']
@@ -190,9 +145,7 @@ def count_influx(request):
     return HttpResponse(response)
 
 def fetch_from_influx(request):
-
     query = f'from(bucket:"{databucket}")|> range(start: -30d)|> filter(fn: (r) => r._measurement == "objectDetection")|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")|> sort(columns: ["_time"], desc: true)|>limit(n: 10)'
-
     result = client.query_api().query(query)
     json_result = []
     for table in result:
@@ -203,9 +156,39 @@ def fetch_from_influx(request):
             # Add time field to record_values dictionary
             record_values['time'] = record_time
             json_result.append(record_values)
-
     # Return the JSON object as a web response
     response = json.dumps(json_result, default=str)
-
     # Return a success response
     return JsonResponse(response,safe=False)
+
+
+
+
+# --------------
+
+import threading
+
+# Define a function to be executed in the separate thread
+def my_function():
+    # Your code goes here...
+    import schedule
+    import time
+    def job():
+        print("I'm working...")
+    schedule.every().day.at("14:50").do(job)
+    
+    def job_with_argument(name):
+        print(f"I am {name}")
+
+    schedule.every(10).seconds.do(job_with_argument, name="Peter")
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+    print("Running in the separate thread.")
+
+# Create a thread object with your function
+my_thread = threading.Thread(target=my_function)
+
+# Start the thread
+my_thread.start()
