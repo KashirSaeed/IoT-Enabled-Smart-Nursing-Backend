0abbe02fec4a2b1b861eb74ec073de5fef6176e8
AbdulAhad696 <abdulahad_212@outlook.com>
2023-06-19 23:08:40 +0500

    Update wsgi.py

--- smartNursingBackend/wsgi.py
+++ smartNursingBackend/wsgi.py
@@ -8,18 +8,16 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 import os
 import atexit
-# from smartNursingBackend.pusherClient import initPusher,killPusher
+from smartNursingBackend.pusherClient import initPusher,killPusher
 from smartNursingBackend.mqttReceive import HiveMqtt
 from django.core.wsgi import get_wsgi_application
 from smartNursingBackend.myLogger import myThread1
 
 
-
-
 def cleanup_function():
-    mqinstance = HiveMqtt()
-    mqinstance.killClient()
-    # killPusher()
+    # mqinstance = HiveMqtt()
+    # mqinstance.killClient()
+    killPusher()
     print("Application terminated. Performing cleanup.")
 
 
@@ -29,7 +27,7 @@ atexit.register(cleanup_function)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartNursingBackend.settings')
 
 app = get_wsgi_application()
-# initPusher()
-mqinstance = HiveMqtt()
+initPusher()
+# mqinstance = HiveMqtt()
 # Start the thread
 myThread1.start()
